6ec88f001d37bfdfe4b7799c646ac9bf26f8b58c
Juan <caballerof204@gmail.com>
2022-04-16 23:26:14 -0500

    add the search

--- src/test/java/com/amazon/stepdefinitions/Steps.java
+++ src/test/java/com/amazon/stepdefinitions/Steps.java
@@ -1,23 +1,36 @@
 package com.amazon.stepdefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class Steps {
 
     private WebDriver driver;
-    @Given("juan navigates to www.amazon.com")
-    public void juan_navigates_to_www_amazon_com() {
+    private Actions action;
+
+    @Before()
+    public void setUp(){
         System.setProperty("webdriver.chrome.driver","/home/nana/Descargas/chromedriver");
         driver= new ChromeDriver();
-        driver.get("https://www.amazon.com/");
-        System.out.println("open chrome");
+        action = new Actions(driver);
+    }
+    @Given("juan navigates to www.amazon.com")
+    public void juan_navigates_to_www_amazon_com() throws InterruptedException {
+        driver.get("https://www.amazon.com/?language=es_US");
+        Thread.sleep(3000);
     }
     @Given("Searches for Alexa")
-    public void searches_for_alexa() {
-        System.out.println("search for alexa");
+    public void searches_for_alexa() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("Alexa");
+        driver.findElement(By.xpath("//*[@id=\"nav-search-submit-button\"]")).submit();
+        Thread.sleep(3000);
     }
     @Given("navigates to the second page")
     public void navigates_to_the_second_page() {
@@ -30,5 +43,10 @@ public class Steps {
     @Then("assert that the item would be available for purchase")
     public void assert_that_the_item_would_be_available_for_purchase() {
         System.out.println("assert");
+    }
+
+    @After()
+    public void finish(){
+        driver.quit();
     }
 }
